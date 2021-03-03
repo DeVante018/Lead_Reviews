b96be89abe02aa21ef132158922491cd9166f5b7
DeVante018 <devantef@buffalo.edu>
2021-03-02 19:32:30 -0500

    Made skeleton for receiving passwords and movie likes with user hash

--- src/server.py
+++ src/server.py
@@ -32,16 +32,39 @@ def parse_request():
     print(username, password)
 
     if usr_exist and valid_password:
-        return redirect("/login/"+username)
+        return redirect("/login/" + username)
     else:
         return error_response()
-    #check if user is in the data base
-    #if user exist check they have the correct password
+    # check if user is in the data base
+    # if user exist check they have the correct password
 
 
 def error_response():
-        response = ""
-        return response
+    response = ""
+    return response
+
+
+@app.route("/user/like/movie", methods=['POST'])
+def like_movie(movie_id, movie_name, user_name, user_hash):
+    value = 0
+    check_hash = check_user_hash(user_name, user_hash)
+    # make sql request to database
+    if check_hash == 1:
+        kjbi = 0
+        # do more stuff
+    else:
+        iguv = 0
+        # return current number in database
+
+
+def check_user_hash(username, hash):
+    # make call to database
+    # username would be used to search name in database
+    database_result = "find_user_hash(username)" # this would be the function call
+    if hash == database_result:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
