61efc98e4cd86652489dec5d9de8e073d29a0943
DeVante018 <devantef@buffalo.edu>
2021-03-23 16:32:28 -0400

    Added functionality for searching for a movie and receiving post request for the movie we want

--- src/server.py
+++ src/server.py
@@ -1,16 +1,32 @@
 from flask import Flask, redirect, render_template, request
-
 from make_api_call import Api
+import json
 
 app = Flask(__name__)
 std_home_page = "standard_home_page.html"
 login = "login.html"
 login_home = "login_home _page.html"
+search = "search.html"
 aco = Api()
+
 
 @app.route("/")
 def not_signed_in_home_page():
     return render_template(std_home_page)
+
+
+@app.route("/movies/search")
+def search_page():
+    return render_template(search)
+
+
+@app.route("/movies/search/go", methods=['POST'])
+def search_movie():
+    name_of_movie = request.form['movie']
+    print(name_of_movie)
+    call = aco.make_call_to_server(name_of_movie)
+    parse_movie_response(call)
+    return "done"
 
 
 @app.route("/login")
@@ -42,7 +58,7 @@ def parse_request():
 
 
 def error_response():
-    response = ""
+    response = "Username or password not found"
     return response
 
 
@@ -69,6 +85,24 @@ def check_user_hash(username, hash):
         return 0
 
 
+def parse_movie_response(movie_name):
+    s = json.loads(movie_name)
+    # print(s)
+    movie = s['d'][0]
+    movie_image_info = movie['i']
+    movie_id = movie['id']
+    movie_name = movie['l']
+    movie_rank = movie['rank']
+    movie_stars = movie['s']
+    movie_release_date = movie['y']
+
+    print("Movie image info: ", movie_image_info)
+    print("movie id: ", movie_id)
+    print("movie name: ", movie_name)
+    print("movie rank: ", movie_rank)
+    print("movie stars: ", movie_stars)
+    print("release date: ", movie_release_date)
+
+
 if __name__ == '__main__':
-    json_result = aco.make_call_to_server("titanic")
-    # app.run(port=8080, debug=True)
+    app.run(port=8081, debug=True)
