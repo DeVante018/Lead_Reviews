f236b9166dfb4cd71420461c6b1d8bfc25f92946
DeVante018 <devantef@buffalo.edu>
2021-04-29 13:38:03 -0400

    Added method to flag users as online or offline. Created a way to retrieve all online users. Added a visual 'on' or 'off' one the settings page for the user.

--- src/custom_html_pages.py
+++ src/custom_html_pages.py
@@ -1,6 +1,15 @@
 # all methods for creating html pages for the specific user
+import flask_login
 
-def settings_page() -> str:
+
+def settings_page(database) -> str:
+    do_not_disturb = ""
+    cur_usr = flask_login.current_user
+    user = cur_usr.username
+    data = database.users.find({"username": user})
+    for info in data:
+        do_not_disturb = info['do_not_disturb']
+
     f = open("../templates/settings.html", 'r')
     defalt_img = "../static/images/default_pic.png"
     custom = ""
@@ -10,7 +19,42 @@ def settings_page() -> str:
     for line in f:
         if '{Profile_Image}' in line:
             custom += line.replace('{Profile_Image}', defalt_img)
+        elif '{{On_Off_Indicator}}' in line:
+            if do_not_disturb:
+                custom += line.replace('{{On_Off_Indicator}}', "off")
+            else:
+                custom += line.replace('{{On_Off_Indicator}}', "on")
         else:
             custom += line
 
     return custom
+
+
+def get_online_users(cur_user, database):
+    online = database.online.find()
+    is_online = []
+    for status in online:
+        if cur_user != status['username'] and status['status'] == 'online':
+            is_online.append(status['username'])
+    return is_online
+
+
+def set_online(username, database):
+    database.online.update_one(
+        {"username": username},
+        {
+            "$set": {
+                "status": "online"}
+        }
+    )
+
+
+def set_offline(username, database):
+    database.online.update_one(
+        {"username": username},
+        {
+            "$set": {
+                "status": "offline"}
+        }
+    )
+
